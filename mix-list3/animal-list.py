777aed15d0ec3056bcf3f316b03779149c630624
vudayagiri <venkat.vudayagiri@verizon.com>
2019-05-13 17:16:06 -0400

    mix list 3

---
+++ mix-list3/animal-list.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+animals = ["Fox","Fly","Ant","bee","cod","cat","dog","yak","cow","hen","koi","hog","jay","kit"]
+print(animals)
+print(animals[3])
+animals.append("tik")
+print(animals)
+ani = ["dogs","cats","cows"]
+animals.extend(ani)
+print(animals)
+animals.append(ani)
+print(animals)
